a83fe22c2d8894b4b801525f66357689ee82042a
KarenKaoK <karen.kao.2023@gmail.com>
2026-07-31 22:08:36 +0200

    style: format code with black

--- src/preprocess/transforms.py
+++ src/preprocess/transforms.py
@@ -1,15 +1,16 @@
-import torch 
+import torch
 from torchvision.transforms import v2
+
 
 def build_transforms():
     return v2.Compose(
         [
-        v2.Resize((32,32)),
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize(
-            mean=[0.5, 0.5, 0.5],
-            std=[0.5, 0.5, 0.5],
+            v2.Resize((32, 32)),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(
+                mean=[0.5, 0.5, 0.5],
+                std=[0.5, 0.5, 0.5],
             ),
         ]
     )
--- tests/unit/preprocess/test_transforms.py
+++ tests/unit/preprocess/test_transforms.py
@@ -1,18 +1,15 @@
-
 import torch
-import pytest
-
 from PIL import Image
-from torchvision.transforms import v2
 from src.preprocess.transforms import build_transforms
+
 
 def test_build_transforms():
 
     # arrange
     image_input = Image.new(
         mode="RGB",
-        size=(20,10),
-        color=(255,255,255),   
+        size=(20, 10),
+        color=(255, 255, 255),
     )
 
     transform = build_transforms()
@@ -24,6 +21,3 @@ def test_build_transforms():
     assert image_output.dtype == torch.float32
     assert image_output.min().item() == 1.0
     assert image_output.max().item() == 1.0
-
-
-
